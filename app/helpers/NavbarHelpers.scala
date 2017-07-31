b7d36e84cb65fac95e0fefd99a208050d27e8ceb
adamdye12 <adam.dye@qa.com>
2017-07-31 14:20:27 +0100

    update and delete added

---
+++ app/helpers/NavbarHelpers.scala
@@ -0,0 +1,12 @@
+package helpers
+
+/**
+  * Created by Administrator on 31/07/2017.
+  */
+class NavbarHelpers {
+
+  val homePage = List("class=active", "", "", "")
+  val searchPage = List("","class=active", "", "")
+  val thirdPage = List("","","class=active", "", "", "")
+  val fourthPage = List("","","","class=active")
+}
